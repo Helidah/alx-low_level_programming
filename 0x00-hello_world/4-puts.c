d837c946eee32a004bd2b876d6284950f732d932
Helidah <helidahwgd437@gmail.com>
2021-06-17 12:15:19 -0700

    a C program that prints a quote using putc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints written quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
