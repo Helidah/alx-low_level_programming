4d89936294e3227388901b845fdb063889362f0b
Helidah <helidahwgd437@gmail.com>
2021-06-17 12:25:45 -0700

    a C program that prints a quote using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Prints quote
- *
- * Return: zero on success
- *
+ *main -  print quote using printf
+ *Description: very simple snippet
+ *Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
