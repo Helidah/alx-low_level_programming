045fdca091f3687d0e56fe9361aa893b1e82f4e7
Helidah <helidahwgd437@gmail.com>
2021-07-07 06:18:49 -0700

    Write a function that prints a string in reverse.

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,21 +6,11 @@
  * Return: factorial of the number.
  */
 int factorial(int n)
-
 {
-int result;
-
-if (n < 0)
-{
-result = -1;
-}
-else if (n == 0)
-{
-result = 1;
-}
-else
-{
-result = n * factorial (n - 1);
-}
-return result;
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
