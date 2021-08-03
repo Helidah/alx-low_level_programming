9b6aa3bf8feb99d98a8f7263a5944d02e9c500ad
Helidah <helidahwgd437@gmail.com>
2021-08-03 09:01:17 -0700

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,8 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 }	listint_t;
+
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -30,5 +32,8 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+size_t looped_listint_count(listint_t *head);
+
 
 #endif
