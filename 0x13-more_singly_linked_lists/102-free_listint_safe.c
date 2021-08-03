b2b30024c64c05f8718f710e251e3bb6ec7d4aa7
Helidah <helidahwgd437@gmail.com>
2021-08-03 09:28:04 -0700

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -6,22 +6,6 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
-
-	count = 0;
-	current = *h;
-	while (current != NULL)
-	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
-
-		if (hold < current)
-			break;
-	}
-	*h = NULL;
-
-	return (count);
+	h++;
+	return (0);
 }
