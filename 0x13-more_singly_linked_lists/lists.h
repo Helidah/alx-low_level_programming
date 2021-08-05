31dfa1fe3ab27c338cbbb2247faec4d5afdc1bd0
Helidah <helidahwgd437@gmail.com>
2021-08-05 04:46:45 -0700

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,12 +9,12 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-}	listint_t;
 
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
